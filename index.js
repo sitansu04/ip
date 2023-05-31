3f487468e4c31ecb5d6c5ea8ec7c02ba54456176
Sitansu Mandal <sitansugcelt@gmail.com>
2023-05-31 12:25:17 +0530

    v3.0.0.0

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const unirest = require("unirest");
+const os = require("os");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    const apiCall = unirest(
-              "GET",
-              "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
-            );
-            apiCall.headers({
-              "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-              "x-rapidapi-key": "a8e7d82e4dmsh53e8c803f3e5ebep122fb5jsnc150bf37d498",
-            });
-            apiCall.end(function (result) {
-              if (res.error) throw new Error(result.error);
-              console.log(result.body);
-              res.json(result.body.ip);
-            });
+  function getDeviceIP() {
+    const networkInterfaces = os.networkInterfaces();
+
+    for (const interfaceName in networkInterfaces) {
+      const interfaces = networkInterfaces[interfaceName];
+
+      for (const iface of interfaces) {
+        // Skip over non-IPv4 and internal (e.g., loopback) addresses
+        if (iface.family === "IPv4" && !iface.internal) {
+          return iface.address;
+        }
+      }
+    }
+
+    return "Device IP not found";
+  }
+
+  // Usage
+  const deviceIP = getDeviceIP();
+  res.send({Device_IP: deviceIP});
 });
 
 app.listen(8080, () => {
